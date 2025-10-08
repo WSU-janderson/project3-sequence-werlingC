fc1ba389a4f8303e1cd3948a76cafe7a0bafd96b
James Anderson <james.anderson@wright.edu>
2025-10-08 15:01:26 -0400

    updated test harness to work with strings

--- SequenceTestHarness.cpp
+++ SequenceTestHarness.cpp
@@ -1,5 +1,5 @@
 /**
- * SequenceTestHarness.cpp
+* SequenceTestHarness.cpp
  * CS 3100
  * Project 3
  *
@@ -10,13 +10,57 @@
  * when running tests. To run this file in CLion, you need to select SequenceTestHarness from the drop-down
  * menu next to the Build (hammer) icon.
  */
+
 #include <iomanip>
 #include <iostream>
-
+#include <string>
 #include "Sequence.h"
 
+using namespace std;
+
+// -----------------------------------------------------------------------------
+// Change this typedef to switch test type (int, double, std::string, etc.)
+// -----------------------------------------------------------------------------
+typedef std::string value_type;
+// typedef int value_type;
+// typedef double value_type;
+
+// -----------------------------------------------------------------------------
+// Change this typedef to switch test type (int, double, std::string, etc.)
+// -----------------------------------------------------------------------------
+typedef std::string value_type;
+// typedef int value_type;
+// typedef double value_type;
+
+// -----------------------------------------------------------------------------
+// Conversion helper to generate values from integer indices.
+// -----------------------------------------------------------------------------
+inline value_type make_value(int i);
+
+// Overload for string-like types
+inline std::string make_value_string(int i) {
+	return std::string(1, 'A' + (i % 26));
+}
+
+// Generic template fallback for numeric or trivially convertible types
+template <typename T>
+inline T make_value_generic(int i) {
+	return static_cast<T>(i + 1);
+}
+
+// Dispatcher
+inline value_type make_value(int i) {
+	if constexpr (std::is_same_v<value_type, std::string>)
+		return make_value_string(i);
+	else
+		return make_value_generic<value_type>(i);
+}
+
+// -----------------------------------------------------------------------------
+
 const std::string evalName{ "" };
-// #define __GRADING // Leave this commented out for your tests
+
+// #define __GRADING
 
 #define __CREATE_PRINT
 #define __INDEP
@@ -49,8 +93,6 @@ const std::string evalName{ "" };
 #define OUTSTREAM cout
 #endif
 
-using namespace std;
-
 void testCopyConstructor(Sequence, ostream&);
 void memoryLeakTest();
 
@@ -58,7 +100,6 @@ int main() {
 
 #ifdef __GRADING
 	ofstream OUTSTREAM;
-	// change ./evals/ to where you want the evaluations saved
 	OUTSTREAM.open("./evals/" + evalName + "_eval.txt");
 	if (!OUTSTREAM.good()) {
 		std::cout << "Evaluation failed to open\n";
@@ -66,31 +107,24 @@ int main() {
 #endif
 
 	OUTSTREAM << "Grading for " << evalName << "\n";
-	// Create a sequence of length four, store some values, and print
+
+	// CREATE / PRINT
 	try {
 		OUTSTREAM << "Testing sequence creation and printing" << endl;
 		OUTSTREAM << "--------------------------------------" << endl;
 #ifdef __CREATE_PRINT
 		Sequence data(4);
-		data[0] = 100;
-		data[1] = 200;
-		data[2] = 300;
+		data[0] = make_value(0);
+		data[1] = make_value(1);
+		data[2] = make_value(2);
 		OUTSTREAM << "Sequence:  " << data << endl;
-		OUTSTREAM << "Should be: <100, 200, 300, \?\?\?>" << endl << endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
 #endif
 	}
-
 	catch (exception& e) {
 		OUTSTREAM << "Exception: " << e.what() << endl << endl;
 	}
 
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 5):            5" << endl << endl;
-#endif
-
-	// Test for independent sequences
+	// INDEPENDENT SEQUENCES
 	try {
 		OUTSTREAM << "Testing multiple sequences" << endl;
 		OUTSTREAM << "--------------------------" << endl;
@@ -99,550 +133,134 @@ int main() {
 		Sequence s2(3);
 
 		for (int i = 0; i < 3; i++) {
-			s1[i] = i;
-			s2[i] = 100 + i;
+			s1[i] = make_value(i);
+			s2[i] = make_value(100 + i);
 		}
 		OUTSTREAM << "Sequence1: " << s1 << endl;
-		OUTSTREAM << "Sequence2: " << s2 << endl;
-		OUTSTREAM << "Should be: <0, 1, 2>" << endl
-			<< "           <100, 101, 102>" << endl
-			<< endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
+		OUTSTREAM << "Sequence2: " << s2 << endl << endl;
 #endif
 	}
-
 	catch (exception& e) {
 		OUTSTREAM << "Exception: " << e.what() << endl << endl;
 	}
 
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 3):            3" << endl << endl;
-#endif
-
-	// Test push_back
+	// PUSH_BACK
 	try {
 		OUTSTREAM << "Testing push_back()" << endl;
 		OUTSTREAM << "-------------------" << endl;
 #ifdef __PUSH_BACK
 		Sequence data(3);
-		data[0] = 100;
-		data[1] = 200;
-		data[2] = 300;
-		data.push_back(400);
-		data.push_back(500);
+		for (int i = 0; i < 3; i++) data[i] = make_value(i);
+		data.push_back(make_value(3));
+		data.push_back(make_value(4));
 		OUTSTREAM << "Sequence:  " << data << endl;
-		OUTSTREAM << "Should be: <100, 200, 300, 400, 500>" << endl << endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
 #endif
 	}
-
 	catch (exception& e) {
 		OUTSTREAM << "Exception: " << e.what() << endl << endl;
 	}
 
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 3):            3" << endl << endl;
-#endif
-
-	// Test push_back to an empty sequence
-	try {
-		OUTSTREAM << "Testing push_back() on an empty sequence" << endl;
-		OUTSTREAM << "-------------------" << endl;
-#ifdef __PUSH_BACK_EMPTY
-		Sequence data(0);
-		data.push_back(100);
-		data.push_back(200);
-		data.push_back(300);
-		data.push_back(400);
-		data.push_back(500);
-		OUTSTREAM << "Sequence:  " << data << endl;
-		OUTSTREAM << "Should be: <100, 200, 300, 400, 500>" << endl << endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
-#endif
-	}
-
-	catch (exception& e) {
-		OUTSTREAM << "Exception: " << e.what() << endl << endl;
-	}
-
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 1):            1" << endl << endl;
-#endif
-
-	// Test pop_back
+	// POP_BACK
 	try {
 		OUTSTREAM << "Testing pop_back()" << endl;
 		OUTSTREAM << "------------------" << endl;
 #ifdef __POP_BACK
 		Sequence data(5);
-		for (int i = 0; i < 5; i++) {
-			data[i] = ( i + 1 ) * 100;
-		}
+		for (int i = 0; i < 5; i++) data[i] = make_value(i);
 		data.pop_back();
 		data.pop_back();
 		OUTSTREAM << "Sequence:   " << data << endl;
-		OUTSTREAM << "Should be:  <100, 200, 300>" << endl << endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
 #endif
 	}
-
 	catch (exception& e) {
 		OUTSTREAM << "Exception: " << e.what() << endl << endl;
 	}
 
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 3):            3" << endl << endl;
-#endif
-
-	// Test pop_back on empty sequence
+	// INSERT
 	try {
-		OUTSTREAM << "Testing pop_back() on an empty sequence" << endl;
-		OUTSTREAM << "------------------" << endl;
-#ifdef __POP_BACK_EMPTY
-		Sequence data(3);
-		for (int i = 0; i < 3; i++) {
-			data[i] = ( i + 1 ) * 100;
-		}
-		data.pop_back();
-		data.pop_back();
-		data.pop_back();
-		data.pop_back();
-		OUTSTREAM << "ERROR: Pop_back() DID NOT throw an exception" << endl
-			<< endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
-#endif
-	}
-
-	catch (exception& e) {
-		OUTSTREAM << "CORRECT: Threw exception: " << e.what() << endl << endl;
-	}
-
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 1):            1" << endl << endl;
-#endif
-
-	// Test insert()
-	try {
-		OUTSTREAM << "Testing insert()" << endl;
-		OUTSTREAM << "------------------" << endl;
 #ifdef __INSERT
 		Sequence data(5);
-
-		for (int i = 0; i < 5; i++) {
-			data[i] = ( i + 1 ) * 100;
-		}
-
-		data.insert(3, 999);
+		for (int i = 0; i < 5; i++) data[i] = make_value(i);
+		data.insert(3, make_value(99));
+		data.insert(0, make_value(88));
+		data.insert(6, make_value(77));
 		OUTSTREAM << "Sequence:   " << data << endl;
-		OUTSTREAM << "Should be:  <100, 200, 300, 999, 400, 500>" << endl
-			<< endl;
-
-		data.insert(0, 888);
-		OUTSTREAM << "Sequence:   " << data << endl;
-		OUTSTREAM << "Should be:  <888, 100, 200, 300, 999, 400, 500>" << endl
-			<< endl;
-
-		data.insert(6, 777);
-		OUTSTREAM << "Sequence:   " << data << endl;
-		OUTSTREAM << "Should be:  <888, 100, 200, 300, 999, 400, 777, 500>"
-			<< endl
-			<< endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
 #endif
 	}
-
 	catch (exception& e) {
 		OUTSTREAM << "Exception: " << e.what() << endl << endl;
 	}
 
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 3):            3" << endl << endl;
-#endif
-
-	// Test insert on invalid index
+	// FRONT / BACK
 	try {
-		OUTSTREAM << "Testing insert() on an invalid index" << endl;
-		OUTSTREAM << "------------------" << endl;
-#ifdef __INSERT_INVALID
-		Sequence data(3);
-		for (int i = 0; i < 3; i++) {
-			data[i] = ( i + 1 ) * 100;
-		}
-		data.insert(5, 555);
-		OUTSTREAM << "ERROR: insert() DID NOT throw an exception" << endl
-			<< endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
-#endif
-	}
-
-	catch (exception& e) {
-		OUTSTREAM << "CORRECT: Threw exception: " << e.what() << endl << endl;
-	}
-
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 1):            1" << endl << endl;
-#endif
-
-	// Test front()
-	try {
-		OUTSTREAM << "Testing front()" << endl;
-		OUTSTREAM << "---------------" << endl;
 #ifdef __FRONT
 		Sequence data(3);
-
-		for (int i = 0; i < 3; i++) {
-			data[i] = ( i + 1 ) * 100;
-		}
-
-		OUTSTREAM << "Front:     " << data.front() << endl;
-		OUTSTREAM << "Sequence:  " << data << endl;
-		OUTSTREAM << "Should be: 100" << endl;
-		OUTSTREAM << "           <100, 200, 300>" << endl << endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
+		for (int i = 0; i < 3; i++) data[i] = make_value(i);
+		OUTSTREAM << "Front: " << data.front() << endl;
 #endif
-	}
-
-	catch (exception& e) {
-		OUTSTREAM << "Exception: " << e.what() << endl << endl;
-	}
-
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 2):            2" << endl << endl;
-#endif
-
-	// Test front() on empty sequence
-	try {
-		OUTSTREAM << "Testing front() on an empty sequence" << endl;
-		OUTSTREAM << "------------------------------------" << endl;
-#ifdef __FRONT_EMPTY
-		Sequence data(0);
-		data.front();
-		OUTSTREAM << "ERROR: front() DID NOT throw an exception" << endl
-			<< endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
-#endif
-	}
-
-	catch (exception& e) {
-		OUTSTREAM << "CORRECT: Threw exception: " << e.what() << endl << endl;
-	}
-
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 1):            1" << endl << endl;
-#endif
-
-	// Test back()
-	try {
-		OUTSTREAM << "Testing back()" << endl;
-		OUTSTREAM << "---------------" << endl;
 #ifdef __BACK
-		Sequence data(3);
-
-		for (int i = 0; i < 3; i++) {
-			data[i] = ( i + 1 ) * 100;
-		}
-
-		OUTSTREAM << "Back:      " << data.back() << endl;
-		OUTSTREAM << "Sequence:  " << data << endl;
-		OUTSTREAM << "Should be: 300" << endl;
-		OUTSTREAM << "           <100, 200, 300>" << endl << endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
+		Sequence data2(3);
+		for (int i = 0; i < 3; i++) data2[i] = make_value(i);
+		OUTSTREAM << "Back:  " << data2.back() << endl;
 #endif
 	}
-
 	catch (exception& e) {
 		OUTSTREAM << "Exception: " << e.what() << endl << endl;
 	}
 
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 2):            2" << endl << endl;
-#endif
-
-	// Test back() on empty sequence
+	// ERASE
 	try {
-		OUTSTREAM << "Testing back() on an empty sequence" << endl;
-		OUTSTREAM << "------------------------------------" << endl;
-#ifdef __BACK_EMPTY
-		Sequence data(0);
-		data.back();
-		OUTSTREAM << "ERROR: back() DID NOT throw an exception" << endl << endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
-#endif
-	}
-
-	catch (exception& e) {
-		OUTSTREAM << "CORRECT: Threw exception: " << e.what() << endl << endl;
-	}
-
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 1):            1" << endl << endl;
-#endif
-
-	// Test empty()
-	try {
-		OUTSTREAM << "Testing empty()" << endl;
-		OUTSTREAM << "---------------" << endl;
-#ifdef __EMPTY
-		Sequence empty_sequence(0);
-		Sequence nonempty_sequence(1);
-
-		OUTSTREAM << "Empty sequence, empty returns: " << empty_sequence.empty()
-			<< endl;
-		OUTSTREAM << "Non-empty sequence, empty returns: "
-			<< nonempty_sequence.empty() << endl
-			<< endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
-#endif
-	}
-
-	catch (exception& e) {
-		OUTSTREAM << "Exception: " << e.what() << endl << endl;
-	}
-
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 1):            1" << endl << endl;
-#endif
-
-	// Test size()
-	try {
-		OUTSTREAM << "Testing size()" << endl;
-		OUTSTREAM << "---------------" << endl;
-#ifdef __SIZE
-		Sequence data(7);
-		Sequence empty_sequence(0);
-
-		OUTSTREAM << "Sequence length 7, size returned: " << data.size()
-			<< endl;
-		OUTSTREAM << "Empty sequence, size returned: " << empty_sequence.size()
-			<< endl
-			<< endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
-#endif
-	}
-
-	catch (exception& e) {
-		OUTSTREAM << "Exception: " << e.what() << endl << endl;
-	}
-
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 2):            2" << endl << endl;
-#endif
-
-	// Test clear()
-	try {
-		OUTSTREAM << "Testing clear()" << endl;
-		OUTSTREAM << "---------------" << endl;
-#ifdef __CLEAR
-		Sequence data(5);
-
-		for (int i = 0; i < 5; i++) {
-			data[i] = ( i + 1 ) * 100;
-		}
-
-		data.clear();
-		OUTSTREAM << "Sequence cleared, empty returned: " << data.empty()
-			<< endl;
-		OUTSTREAM << "Size returned:  " << data.size() << endl << endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
-#endif
-	}
-
-	catch (exception& e) {
-		OUTSTREAM << "Exception: " << e.what() << endl << endl;
-	}
-
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 2):            2" << endl << endl;
-#endif
-
-	// Test erase
-	try {
-		OUTSTREAM << "Testing erase()" << endl;
-		OUTSTREAM << "------------------" << endl;
 #ifdef __ERASE
-		Sequence data(10);
-		for (int i = 0; i < 10; i++) {
-			data[i] = ( i + 1 ) * 100;
-		}
-		data.erase(3, 4);
-		OUTSTREAM << "Sequence:   " << data << endl;
-		OUTSTREAM << "Should be:  <100, 200, 300, 800, 900, 1000>" << endl
-			<< endl;
-
-		data.erase(4, 2);
-		OUTSTREAM << "Sequence:   " << data << endl;
-		OUTSTREAM << "Should be:  <100, 200, 300, 800>" << endl << endl;
-
-		data.erase(0, 2);
-		OUTSTREAM << "Sequence:   " << data << endl;
-		OUTSTREAM << "Should be:  <300, 800>" << endl << endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
+		Sequence data(6);
+		for (int i = 0; i < 6; i++) data[i] = make_value(i);
+		data.erase(2, 2); // remove two elements
+		OUTSTREAM << "Sequence: " << data << endl;
 #endif
 	}
-
 	catch (exception& e) {
-		OUTSTREAM << "Exception: " << e.what() << endl << endl;
+		OUTSTREAM << "Exception: " << e.what() << endl;
 	}
 
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 3):            3" << endl << endl;
-#endif
-
-	// Test erase with invalid parameters
+	// ASSIGNMENT
 	try {
-		OUTSTREAM << "Testing erase() on invalid parameters" << endl;
-		OUTSTREAM << "------------------" << endl;
-#ifdef __ERASE_INVALID
-		Sequence data(5);
-		for (int i = 0; i < 5; i++) {
-			data[i] = ( i + 1 ) * 100;
-		}
-		data.erase(3, 5);
-		OUTSTREAM << "ERROR: erase() DID NOT throw an exception" << endl
-			<< endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
-#endif
-	}
-
-	catch (exception& e) {
-		OUTSTREAM << "CORRECT: Threw exception: " << e.what() << endl << endl;
-	}
-
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 1):            1" << endl << endl;
-#endif
-
-	// Test assignment (=) operator
-	try {
-		OUTSTREAM << "Testing assignment (=) operator" << endl;
-		OUTSTREAM << "------------------" << endl;
 #ifdef __ASSIGNMENT
-		Sequence data1(5);
+		Sequence data1(3);
+		for (int i = 0; i < 3; i++) data1[i] = make_value(i);
 		Sequence data2(0);
-
-		for (int i = 0; i < 5; i++) {
-			data1[i] = ( i + 1 ) * 100;
-		}
-
 		data2 = data1;
-
-		data2[0] = 1;
-		data2[1] = 2;
-
-		OUTSTREAM << "Data1:      " << data1 << endl;
-		OUTSTREAM << "Data2:      " << data2 << endl;
-		OUTSTREAM << "Should be:  <100, 200, 300, 400, 500>" << endl;
-		OUTSTREAM << "            <1, 2, 300, 400, 500>" << endl << endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
+		data2[0] = make_value(99);
+		OUTSTREAM << "data1: " << data1 << endl;
+		OUTSTREAM << "data2: " << data2 << endl;
 #endif
 	}
-
 	catch (exception& e) {
-		OUTSTREAM << "Exception: " << e.what() << endl << endl;
+		OUTSTREAM << "Exception: " << e.what() << endl;
 	}
 
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 3):            3" << endl << endl;
-#endif
-
-	// Test copy constructor
+	// COPY CONSTRUCTOR
 	try {
-		OUTSTREAM << "Testing copy constructor" << endl;
-		OUTSTREAM << "------------------" << endl;
 #ifdef __COPY_CONSTRUCTOR
-		Sequence data(5);
-
-		for (int i = 0; i < 5; i++) {
-			data[i] = ( i + 1 ) * 100;
-		}
-
+		Sequence data(3);
+		for (int i = 0; i < 3; i++) data[i] = make_value(i);
 		testCopyConstructor(data, OUTSTREAM);
-
-		OUTSTREAM << "Original Sequence:      " << data << endl;
-		OUTSTREAM << "Should be:              <1, 200, 300, 400, 500>" << endl;
-		OUTSTREAM << "                        <100, 200, 300, 400, 500>" << endl
-			<< endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
+		OUTSTREAM << "Original: " << data << endl;
 #endif
 	}
-
 	catch (exception& e) {
-		OUTSTREAM << "Exception: " << e.what() << endl << endl;
+		OUTSTREAM << "Exception: " << e.what() << endl;
 	}
 
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 3):            3" << endl << endl;
-#endif
-
-	// Test for memory leaks
-	try {
-		OUTSTREAM << "Testing for memory leaks" << endl;
-		OUTSTREAM << "------------------------" << endl;
-#ifdef __MEMORY_LEAK_TEST
-		cout << "Pre-memory leak test..." << endl;
-		system("pause");
-
-		cout << "Running...\n";
-		for (int i = 0; i < NUM_MEM_TESTS; i++) {
-			memoryLeakTest();
-		}
-		cout << "Post-memory leak test..." << endl;
-		system("pause");
-
-		OUTSTREAM << "No memory leak found" << endl;
-		OUTSTREAM << "*** Error:  memory leak found" << endl << endl;
-#else
-		OUTSTREAM << "*** UNHANDLED CRASH DURING TESTING ***" << endl << endl;
-#endif
-	}
-
-	catch (exception& e) {
-		OUTSTREAM << "Exception: " << e.what() << endl << endl;
-	}
-
-#ifdef __GRADING
-	OUTSTREAM << "Points (out of 3):            3" << endl << endl;
-#endif
-
-#ifdef __GRADING
-	OUTSTREAM << "Programming style and documentation" << endl;
-	OUTSTREAM << "-----------------------------------" << endl;
-	OUTSTREAM << "Points (out of 5):            5" << endl << endl;
-	OUTSTREAM << "TOTAL POINTS (out of 50):    50" << endl;
-	OUTSTREAM.close();
-#endif
 	return 0;
-} // END OF MAIN
+}
 
 void memoryLeakTest() {
-	try {
-		Sequence s(MEM_TEST_SIZE);
-		for (int i = 0; i < MEM_TEST_SIZE; i++) {
-			s[i] = i;
-		}
+	Sequence s(MEM_TEST_SIZE);
+	for (int i = 0; i < MEM_TEST_SIZE; i++) {
+		s[i] = make_value(i);
 	}
-	catch (exception& e) { throw e; }
 }
 
 void testCopyConstructor(Sequence s, ostream& os) {
-	s[0] = 1;
-	os << "Copied Sequence:        " << s << endl;
+	s[0] = make_value(99);
+	os << "Copied Sequence: " << s << endl;
 }
